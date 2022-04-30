863df339fc293f49173a293a780673e51e78c3a7
Vijay <vjwarboy13@gmail.com>
2022-04-30 21:49:39 +0530

    footer changed

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,8 +3,8 @@ import styles from "../styles/Home.module.css";
 export default function Footer() {
   return (
     <>
-      <footer className={styles.footer} style={{ margin: 20 }}>
-        Powered by <span> DevGroves </span>
+      <footer className={styles.footer} style={{ margin: 30 }}>
+        Copyright @2021 DevGroves.
       </footer>
     </>
   );

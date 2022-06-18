3c25cf17530b56f86332364b52f40310ba04c4bc
Vijay <vjwarboy13@gmail.com>
2022-06-18 15:56:49 +0530

    contact from layout added

--- pages/contact.jsx
+++ pages/contact.jsx
@@ -6,6 +6,7 @@ import { Container, Row, Col } from "react-bootstrap";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import Breadcrumbs from "../components/Breadcrumbs";
+import FormImage from "./images/contact-form.png"
 
 export default function Contact() {
   return (
@@ -21,8 +22,15 @@ export default function Contact() {
       <Header />
       <Container>
         <Row className="center container-height">
-        <Col md={6} sm={12} className="mobile-container">
-            <Image src={ContactUsImage} alt="Contact us image" height={400} width={600} placeholder="blur" className="imgContain" />
+          <Col md={6} sm={12} className="mobile-container">
+            <Image
+              src={ContactUsImage}
+              alt="Contact us image"
+              height={400}
+              width={600}
+              placeholder="blur"
+              className="imgContain"
+            />
             <Breadcrumbs name="Contact us" />
           </Col>
           <Col className="zeropadding" md={6} sm={12}>
@@ -32,6 +40,35 @@ export default function Contact() {
             <h4>Contact Us:</h4> +91-96292-30494
             <br />
             <br />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="center">
+            <Image src={FormImage}  alt=" form image" />
+          </Col>
+          <Col>
+            <div className="form-container">
+              <form>
+                <label htmlFor="fname">Name</label>
+                <input type="text" id="fname" name="firstname" placeholder="Your name.." />
+
+                <label htmlFor="number">Contact Number</label>
+                <input type="text" id="number" name="number" placeholder="Your number" />
+
+                <label htmlFor="email">Email</label>
+                <input type="email" id="email" name="email" placeholder="Your email" />
+
+                <label htmlFor="file">Attachment</label>
+                <input type="file" id="file" name="file" placeholder="Attach Your file" />
+
+                <label htmlFor="subject">Description</label>
+                <textarea id="subject" name="subject" placeholder="Write something..">
+                  
+                </textarea>
+
+                <input type="submit" value="Submit" />
+              </form>
+            </div>
           </Col>
         </Row>
       </Container>
